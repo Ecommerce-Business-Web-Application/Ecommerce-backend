e1bab75eec6a893c949067ee8b80850ff7d4538f
pukarsai <85217415+pukarsai@users.noreply.github.com>
2023-11-25 22:55:00 +0545

    First Commit

---
+++ EcommerceDB/EcommerceDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EcommerceDB.Models;
+
+namespace Ecommerce
+{
+    public class EcommerceDbContext : DbContext
+    {
+        public EcommerceDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        DbSet<Category> Categories { get; set; }
+        DbSet<Product> Products { get; set; }
+    }
+}
---
+++ EcommerceDB/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceDB.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        [ForeignKey("Category")]
+        public int categoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
